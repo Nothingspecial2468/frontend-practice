aeda381d7823e61fb9edba10b0db5f7147b495ad
Nothingspecial2468 <167680098+Nothingspecial2468@users.noreply.github.com>
2026-05-06 23:42:15 +0530

    Add logic to the board

--- Drag-And-Drop/script.js
+++ Drag-And-Drop/script.js
@@ -13,6 +13,34 @@ for(const list of lists){
     list.addEventListener("drop", dragDrop)
 }
 
-function dragStart(){
-    
+function dragStart(e){
+    e.dataTransfer.setData("text/plain", this.id)   // it's like a storage box which travels with the dragging process 
+}
+
+function dragEnd(){
+    console.log("Drag ended")
+}
+
+function dragOver(e){
+    e.preventDefault()  // it makes the browser allow dropping
+}
+
+function dragEnter(e){
+    e.preventDefault()
+
+    this.classList.add("over") // it adds the visual effects
+}
+
+function dragLeave(e){
+    this.classList.remove("over")
+}
+
+function dragDrop(e){
+    const id = e.dataTransfer.getData("text/plain") // retrieve stored data
+
+    const card = document.getElementById(id) // get the dragged element
+
+    this.appendChild(card) // move the item into new list
+
+    this.classList.remove("over")
 }
