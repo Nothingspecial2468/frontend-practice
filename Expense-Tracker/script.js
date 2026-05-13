572441830e6cf76c4e7c8658d507f056ebe98c06
Nothingspecial2468 <167680098+Nothingspecial2468@users.noreply.github.com>
2026-05-13 21:46:39 +0530

    Add transactions on the UI

--- Expense-Tracker/script.js
+++ Expense-Tracker/script.js
@@ -5,3 +5,18 @@ const transactionList = document.getElementById("transaction-list")
 const transactionForm = document.getElementById("transaction-form")
 const descriptionEl = document.getElementById("description")
 const amountEl = document.getElementById("amount")
+
+let transactions = JSON.parse(localStorage.getItem("transactions")) || []
+
+transactionForm.addEventListener("submit", addTransaction)
+
+function addTransaction(e){
+    e.preventDefault()
+
+    const description = descriptionEl.ariaValueMax.trim()
+    const amount = parseFloat(amountEl.value)
+
+    transactions.push([])
+}
+
+
