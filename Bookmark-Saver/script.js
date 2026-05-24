35ced397e6b1a5fd517e92d02c94bc84bfaaf742
Nothingspecial2468 <167680098+Nothingspecial2468@users.noreply.github.com>
2026-05-25 00:42:58 +0530

    Add styling and logic

---
+++ Bookmark-Saver/script.js
@@ -0,0 +1,70 @@
+const addBtn = document.getElementById("add-bookmark")
+const bookmarkList = document.getElementById("bookmark-list")
+const bookmarkNameInput = document.getElementById("bookmark-name")
+const bookmarkUrl = document.getElementById("bookmark-url")
+
+document.addEventListener("DOMContentLoaded", loadBookmarks)
+
+addBtn.addEventListener("click", function(){
+    const name = bookmarkNameInput.value.trim()
+    const url = bookmarkUrl.value.trim()
+
+    if(!name || !url){
+        alert("Please enter both name and URL.")
+        return
+    }
+    else{
+        if(!url.startsWith("http://") && !url.startsWith("https://")){
+            alert("Please enter a valid URL starting with http:// or https://")
+            return
+        }
+
+        addBookmark(name,url)
+        saveBookmark(name,url)
+        bookmarkNameInput.value = ""
+        bookmarkUrl.value = ""
+    }
+})
+
+function addBookmark(name, url){
+    const li = document.createElement("li")
+    const link = document.createElement("a")
+    
+    link.href = url
+    link.textContent = name
+    link.target = "_blank"
+
+    const removeBtn = document.createElement("button")
+    removeBtn.textContent = "Remove"
+    removeBtn.addEventListener("click", function(){
+        bookmarkList.removeChild(li)
+        removeBookmarksFromStorage(name, url)
+    })
+
+    li.appendChild(link)
+    li.appendChild(removeBtn)
+
+    bookmarkList.appendChild(li)
+}
+
+function getBookmarksFromStorage(){
+    const bookmarks = localStorage.getItem("bookmarks")
+    return bookmarks ? JSON.parse(bookmarks) : []
+}
+
+function saveBookmark(name, url){
+    const bookmarks = getBookmarksFromStorage()
+    bookmarks.push({name, url})
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
+}
+
+function loadBookmarks(){
+    const bookmarks = getBookmarksFromStorage()
+    bookmarks.forEach((bookmark)=> addBookmark(bookmark.name, bookmark.url))
+}
+
+function removeBookmarksFromStorage(name, url){
+    let bookmarks = getBookmarksFromStorage()
+    bookmarks= bookmarks.filter((bookmark)=> bookmark.name !== name || bookmark.url !== url)
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks))
+}
