94529b6d74f280ce10d3f33d0d79eda784ba0295
Neha <167680098+Nothingspecial2468@users.noreply.github.com>
2026-06-05 19:45:07 +0530

    Add generate password logic

--- Password-Generator/script.js
+++ Password-Generator/script.js
@@ -8,10 +8,44 @@ const numbersCheckbox = document.getElementById("numbers")
 const symbolsCheckbox = document.getElementById("symbols")
 const generateBtn = document.getElementById("generate-btn")
 const strengthBar = document.querySelector(".strength-bar")
-const strengthText = document.querySelector("strength-container p")
+const strengthText = document.querySelector(".strength-container p")
 const strengthLabel = document.getElementById("strength-label")
 
 const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
 const numberCharacters = "0123456789"
 const symbolCharacters = "!@#$%^&*()-_=+[]{}|;:,.<>?/";
+
+lengthSlider.addEventListener("input", ()=>{
+    lengthValue.textContent = lengthSlider.value
+})
+
+generateBtn.addEventListener("click", ()=>{
+    let characterSet = ""
+
+    if(uppercaseCheckbox.checked) characterSet += uppercaseLetters
+    if(lowercaseCheckbox.checked) characterSet += lowercaseLetters
+    if(numbersCheckbox.checked) characterSet += numberCharacters
+    if(symbolsCheckbox.checked) characterSet += symbolCharacters
+
+    passwordInput.value = generatePassword(lengthSlider.value, characterSet)
+})
+
+function generatePassword(length, characterSet){
+    let password = ""
+
+    for(let i = 0; i<length; i++){
+        const randomIndex = Math.floor(Math.random() * characterSet.length)
+        password += characterSet[randomIndex]
+    }
+    return password
+}
+
+copyBtn.addEventListener("click", ()=>{
+    if(passwordInput.value){
+        navigator.clipboard.writeText(passwordInput.value)
+    }
+    else{
+        alert("Please generate a password first!")
+    }
+})
